2f87a0a79db8d717c82eb19157f4946b48f523eb
Kamil Smas <kamil.s@patchstack.com>
2025-08-15 14:11:10 +0200

    initial commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline text-amber-800">
+            Welcome to Laravel + Inertia + Vue!
+        </h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    message: String,
+});
+</script>
